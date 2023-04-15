bc37335e8071812c428516cf057ab7b785c28e39
ChrisSeakane <81533380+ChrisSeakane@users.noreply.github.com>
2023-04-15 13:46:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -24,8 +24,9 @@ app.post(`/validate`, wrap(async (req, res) => {
 
         const options = { headers: { 'Authorization': 'Token OieqQiyzerj9lAbxJzrOm7ULAOxMyyN6DHdLlgdzbxnWzvDphZ' } };
         let response = await got('https://readwise.io/api/v2/auth/', options);
-        
+        console.log(response);
         const resp = JSON.parse(response.body);
+        console.log(resp);
 
         if (token == "OieqQiyzerj9lAbxJzrOm7ULAOxMyyN6DHdLlgdzbxnWzvDphZ") { //replace with real check
             if (req.body.fields.connectionname) {
